beed441fd0198de795cf55b1b52b52f08479e380
samjjc <sam111jc@gmail.com>
2021-03-03 17:35:36 -0500

    initial commit

---
+++ src/main/java/server/ExampleController.java
@@ -0,0 +1,14 @@
+package server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ExampleController {
+    @GetMapping("/")
+    public String greeting(Model model) {
+        return "index";
+    }
+}
