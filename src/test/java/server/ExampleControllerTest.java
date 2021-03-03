257d4c216ac762c12844296bd60d0b7e3f5971e6
samjjc <sam111jc@gmail.com>
2021-03-03 17:37:27 -0500

    add tests

---
+++ src/test/java/server/ExampleControllerTest.java
@@ -0,0 +1,12 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleControllerTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
