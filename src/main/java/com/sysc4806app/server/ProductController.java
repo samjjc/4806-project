2e26e5ead679c4ff2c2f817daa708f537222733d
samjjc <sam111jc@gmail.com>
2021-03-11 18:56:08 -0500

    make product list

---
+++ src/main/java/com/sysc4806app/server/ProductController.java
@@ -0,0 +1,26 @@
+package com.sysc4806app.server;
+
+import com.sysc4806app.model.Product;
+import com.sysc4806app.model.ProductChain;
+import com.sysc4806app.model.ProductType;
+import com.sysc4806app.repos.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    @Autowired
+    private ProductRepo productRepo;
+
+    @GetMapping("/productlist")
+    public String greeting(Model model) {
+
+        model.addAttribute("products", productRepo.findAll());
+
+        return "productlist";
+    }
+}
