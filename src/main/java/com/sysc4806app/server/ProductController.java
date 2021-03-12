803d46d01a97d9e6c3bf9c3fb62e7b21ad637992
Benjamin Melone <melone22@rogers.com>
2021-03-12 00:03:11 -0500

    Added frontend to add products
    Also changed the rest api root to /api and changed the user table to app-user since user is reserved by postgresql

--- src/main/java/com/sysc4806app/server/ProductController.java
+++ src/main/java/com/sysc4806app/server/ProductController.java
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ProductController {
@@ -22,5 +23,19 @@ public class ProductController {
         model.addAttribute("products", productRepo.findAll());
 
         return "productlist";
+    }
+
+    @PostMapping("/product")
+    public RedirectView submitNewProductForm(@ModelAttribute Product product) {
+        productRepo.save(product);
+        return new RedirectView(String.format("/product/%s", product.getId()));
+    }
+
+    @GetMapping(value="/product")
+    public String getNewProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("types", ProductType.values());
+        model.addAttribute("chains", ProductChain.values());
+        return "addNewProductForm";
     }
 }
