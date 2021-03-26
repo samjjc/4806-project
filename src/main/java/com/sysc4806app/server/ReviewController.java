b25fec811f9b8aedd58514fa870b792cfadf36ed
Benjamin Melone <melone22@rogers.com>
2021-03-25 22:35:24 -0400

    Add review form validation and add tests

--- src/main/java/com/sysc4806app/server/ReviewController.java
+++ src/main/java/com/sysc4806app/server/ReviewController.java
@@ -6,11 +6,13 @@ import com.sysc4806app.repos.ReviewRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
 
 @Controller
 public class ReviewController {
@@ -26,8 +28,11 @@ public class ReviewController {
     }
 
     @PostMapping("/product/{id}/review")
-    public RedirectView submitNewProductForm(
-            @PathVariable("id") long id, @ModelAttribute Review review) {
+    public String submitNewProductForm(
+            @PathVariable("id") long id, @Valid @ModelAttribute Review review, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "addNewReviewForm";
+        }
         review.setProduct(productRepo.findById(id));
         review.setId(null);
         reviewRepo.save(review);
@@ -35,7 +40,7 @@ public class ReviewController {
         // or create Anon user that anonymous users can use
         // currently lack of a User is handled in the html
 
-        return new RedirectView(String.format("/product/%s", id));
+        return String.format("redirect:/product/%s", id);
     }
 
     @GetMapping(value="/product/{id}/review")
