2fa893320c46686aa296850178861f770d027108
Eimhin McDonald <Eimh@users.noreply.github.com>
2021-04-03 21:56:44 -0400

    Adds a navigation bar to all of the pages.
    Related to issue #70

---
+++ src/main/resources/static/js/navigation-bar.js
@@ -0,0 +1,42 @@
+class NavBar extends HTMLElement {
+
+    static get observedAttributes() {
+        return ['user'];
+    }
+
+    constructor() {
+        super();
+        this.text = this.textContent;
+    }
+
+    connectedCallback() {
+        if (!this.rendered) {
+            this.render();
+            this.rendered = true;
+        }
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'user') {
+            this.user = newValue;
+        }
+        this.render();
+    }
+
+    render() {
+        var html = '<ul>' +
+            '<li><a href="/"><img src="/logobw.png"/></a></li>' +
+            '<li><a class="ffr-nav-bar-text" href="/productlist">Products</a></li>' +
+            '<li><a class="ffr-nav-bar-text" href="/chains">Chains</a></li>';
+        if (!this.user) {
+            html += '<li class="ffr-nav-bar-right"><a class="ffr-nav-bar-text" href="/signup">Sign Up</a></li>' +
+                '<li class="ffr-nav-bar-right"><a class="ffr-nav-bar-text" href="/login">Login</a></li>';
+        } else {
+            html += '<li><a class="ffr-nav-bar-text" href="/user/' + this.user + '/following">My Follows</a></li>' +
+                '<li class="ffr-nav-bar-right"><a class="ffr-nav-bar-text" href="/signout">Sign Out</a></li>' +
+                '<li class="ffr-nav-bar-right"><a class="ffr-nav-bar-text" href="/user/' + this.user + '">' + this.user + '</a></li>';
+        }
+        this.innerHTML = html + '</ul>';
+    }
+}
+customElements.define('nav-bar', NavBar);
