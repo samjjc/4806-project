a4690b61752950db9d94d10d72ccc646fd9cc39c
Joseph Dooley <josephldooley@gmail.com>
2021-04-05 21:53:49 -0400

    added sort products by average following reviews

--- src/main/java/com/sysc4806app/repos/ReviewRepo.java
+++ src/main/java/com/sysc4806app/repos/ReviewRepo.java
@@ -6,6 +6,7 @@ import com.sysc4806app.model.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ReviewRepo extends CrudRepository<Review, Long> {
@@ -15,5 +16,6 @@ public interface ReviewRepo extends CrudRepository<Review, Long> {
     List<Review> findByUser(User user);
     List<Review> findByRating(int rating);
     List<Review> findByProduct(Product product);
+    List<Review> findByProductAndUserIn(Product product, Collection<User> user);
     List<Review> findByProduct(Product product, Sort sort);
 }
