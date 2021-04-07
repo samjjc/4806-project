f323cdcacc323c665943d898d8f396463f7c077b
Benjamin Melone <melone22@rogers.com>
2021-04-07 19:25:09 -0400

    Add tests for Role Based Auth

--- src/test/java/com/sysc4806app/server/TestSecurityConfig.java
+++ src/test/java/com/sysc4806app/server/TestSecurityConfig.java
@@ -1,18 +1,42 @@
 package com.sysc4806app.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @TestConfiguration
 @Order(-101)
 @EnableWebSecurity
 class TestSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public TestSecurityConfig(BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsService userDetailsService) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+
     @Override
     protected void configure(HttpSecurity security) throws Exception {
         security.httpBasic().and().formLogin().disable().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+        auth.inMemoryAuthentication()
+                .passwordEncoder(bCryptPasswordEncoder)
+                .withUser("testAdmin")
+                .password(bCryptPasswordEncoder.encode("testPass"))
+                .roles("ADMIN");
     }
 }
